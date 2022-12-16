76fbac53ea4d5c19e8334424800e0c48a5ad6ee3
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-16 23:13:02 +0100

    css + ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
 import './style.css'
+
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'] as string[]
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button') as HTMLButtonElement
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+
+  btn.addEventListener('click', () => {
+    stopSongs()
+
+    // @ts-ignore
+    document.getElementById(sound).play()
+  })
+  // @ts-ignore
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound) as HTMLAudioElement
+
+    song.pause()
+    song.currentTime = 0;
+  })
+}
